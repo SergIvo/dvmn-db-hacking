94c8d7ed91b828427fedd0efa31ac892bd23f1d9
Sergey <Sergey@localhost.localdomain>
2022-10-13 22:10:50 +0300

    Initial commit

---
+++ hacking.py
@@ -0,0 +1,62 @@
+from datacenter import models
+import random
+
+
+COMMENDATIONS = [
+    'Молодец!',
+    'Отлично!',
+    'Хорошо!',
+    'Гораздо лучше, чем я ожидал!',
+    'Ты меня приятно удивил!',
+    'Великолепно!',
+    'Прекрасно!',
+    'Ты меня очень обрадовал!',
+    'Именно этого я давно ждал от тебя!',
+    'Сказано здорово – просто и ясно!',
+    'Ты, как всегда, точен!',
+    'Очень хороший ответ!',
+    'Талантливо!',
+    'Ты сегодня прыгнул выше головы!',
+    'Я поражен!',
+    'Уже существенно лучше!',
+    'Потрясающе!',
+    'Замечательно!',
+    'Прекрасное начало!',
+    'Так держать!',
+    'Ты на верном пути!',
+    'Здорово!',
+    'Это как раз то, что нужно!',
+    'Я тобой горжусь!',
+    'С каждым разом у тебя получается всё лучше!',
+    'Мы с тобой не зря поработали!',
+    'Я вижу, как ты стараешься!',
+    'Ты растешь над собой!',
+    'Ты многое сделал, я это вижу!',
+    'Теперь у тебя точно все получится!'
+]
+
+
+
+def fix_marks(schoolkid):
+    bad_grades = models.Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
+    bad_grades.update(points=5)
+
+
+def remove_chastisements(schoolkid):
+    chastisements = models.Chastisement.objects.filter(schoolkid=schoolkid)
+    chastisements.delete()
+
+
+def create_commendation(kid_name, subject_name):
+    schoolkid = models.Schoolkid.objects.get(full_name=kid_name)
+    subject = models.Subject.objects.get(title=subject_name, year_of_study=schoolkid.year_of_study)
+    lessons = models.Lesson.objects.filter(group_letter=schoolkid.group_letter, subject=subject)
+    last_lesson = lessons.order_by('-date').first()
+    text = random.choice(COMMENDATIONS)
+    models.Commendation.objects.create(
+        text=text,
+        created=last_lesson.date,
+        schoolkid=schoolkid,
+        subject=last_lesson.subject,
+        teacher=last_lesson.teacher
+    )
