d2f3e6dc936d8458406ed6694ed51d8dc7e0ff4e
Sergey <Sergey@localhost.localdomain>
2022-10-14 15:11:26 +0300

    Added exception handling and notifications

--- hacking.py
+++ hacking.py
@@ -1,5 +1,7 @@
-from datacenter import models
 import random
+
+from django.core.exceptions import ObjectDoesNotExist
+from datacenter import models
 
 
 COMMENDATIONS = [
@@ -40,16 +42,32 @@ COMMENDATIONS = [
 def fix_marks(schoolkid):
     bad_grades = models.Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
     bad_grades.update(points=5)
+    print('Готово!')
 
 
 def remove_chastisements(schoolkid):
     chastisements = models.Chastisement.objects.filter(schoolkid=schoolkid)
     chastisements.delete()
+    print('Готово!')
 
 
 def create_commendation(kid_name, subject_name):
-    schoolkid = models.Schoolkid.objects.get(full_name=kid_name)
-    subject = models.Subject.objects.get(title=subject_name, year_of_study=schoolkid.year_of_study)
+    try:
+        schoolkid = models.Schoolkid.objects.get(full_name=kid_name)
+    except ObjectDoesNotExist:
+        if kid_name:
+            print(f'Ученик "{kid_name}" не найден в базе. Вероятно, ФИО ученика написаны с ошибкой.')
+        else:
+            print('Ошибка: не указаны Фамилия Имя Отчество ученика.')
+        return
+    try:
+        subject = models.Subject.objects.get(title=subject_name, year_of_study=schoolkid.year_of_study)
+    except ObjectDoesNotExist:
+        if subject_name:
+            print(f'Предмет "{subject_name}" не найден в базе. Вероятно, название предмета написано с ошибкой.')
+        else:
+            print('Ошибка: не указано название предмета.')
+        return
     lessons = models.Lesson.objects.filter(group_letter=schoolkid.group_letter, subject=subject)
     last_lesson = lessons.order_by('-date').first()
     text = random.choice(COMMENDATIONS)
@@ -60,3 +78,4 @@ def create_commendation(kid_name, subject_name):
         subject=last_lesson.subject,
         teacher=last_lesson.teacher
     )
+    print('Готово!')
